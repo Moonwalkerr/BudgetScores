0287acfc25137aa6a1c49a541ba8c6ac2d8778b8
Moonwalker <abhisk.mhra@gmail.com>
2021-05-17 00:14:29 +0530

    firebase login and sign functionallity implemented

--- firebase.js
+++ firebase.js
@@ -6,10 +6,11 @@ auth.onAuthStateChanged((firebaseUser) => {
 });
 const input_email = document.getElementById("email");
 const input_paswd = document.getElementById("paswd");
-const submit_btn = document.getElementById("submit");
+const signIn_btn = document.getElementById("signIn");
 const login_btn = document.getElementById("login");
 
-submit_btn.addEventListener("click", signIn);
+signIn_btn.addEventListener("click", signIn);
+login_btn.addEventListener("click", login);
 
 async function signIn(event) {
   event.preventDefault();
@@ -20,6 +21,19 @@ async function signIn(event) {
       console.log(user);
     });
   } catch (err) {
-    console.log(error);
+    alert.log(err.message);
+  }
+}
+
+async function login(event) {
+  event.preventDefault();
+  const email = input_email.value;
+  const paswd = input_paswd.value;
+  try {
+    await auth
+      .signInWithEmailAndPassword(email, paswd)
+      .then((user) => console.log(user));
+  } catch (err) {
+    alert(err.message);
   }
 }
--- script.js
+++ script.js
@@ -4,51 +4,55 @@ const addButton = document.querySelector("#add_btn");
 const total_expenses = document.querySelector("#total_expenses");
 const resetBtn = document.querySelector("#reset_btn");
 const ul = document.querySelector(".expense_list");
+const logout = document.querySelector("#logout");
 
 const expenseArray = [];
 
 let totalExpenses = 0;
 
 // Adding Expense Button
-addButton.addEventListener("click",()=>{
-    let amountSpent = parseInt(amount_spent_input.value,10);
-    totalExpenses += amountSpent;
-    total_expenses.innerHTML = totalExpenses;
-    expenseArray.push({
-        item:item_name_input.value,
-        amountSpent:amountSpent,
-    })
-    
-    item_name_input.value="";
-    amount_spent_input.value="";
+addButton.addEventListener("click", () => {
+  let amountSpent = parseInt(amount_spent_input.value, 10);
+  totalExpenses += amountSpent;
+  total_expenses.innerHTML = totalExpenses;
+  expenseArray.push({
+    item: item_name_input.value,
+    amountSpent: amountSpent,
+  });
 
+  item_name_input.value = "";
+  amount_spent_input.value = "";
 
-    ul.innerHTML="";
-    expenseArray.forEach(exp=>{
-        const list_item = document.createTextNode(exp.item);
-        const list_amount = document.createTextNode(exp.amountSpent);
-        
-        const span = document.createElement("span");
-        span.classList.add("list-amount");
-        span.appendChild(list_amount);
-        
-        const li = document.createElement("li");
-        li.classList.add("list-group-item");
-        li.appendChild(list_item);
-        li.appendChild(span);     
-        ul.appendChild(li);
-    })
+  ul.innerHTML = "";
+  expenseArray.forEach((exp) => {
+    const list_item = document.createTextNode(exp.item);
+    const list_amount = document.createTextNode(exp.amountSpent);
 
-})
+    const span = document.createElement("span");
+    span.classList.add("list-amount");
+    span.appendChild(list_amount);
 
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.appendChild(list_item);
+    li.appendChild(span);
+    ul.appendChild(li);
+  });
+});
 
 // Reset Button
-resetBtn.addEventListener("click",()=>{
-    expenseArray.length = 0;
-    console.log(expenseArray);
-    ul.innerHTML="";
-    total_expenses.innerHTML="0";
-    item_name_input.value="";
-    amount_spent_input.value="";
+resetBtn.addEventListener("click", () => {
+  expenseArray.length = 0;
+  console.log(expenseArray);
+  ul.innerHTML = "";
+  total_expenses.innerHTML = "0";
+  item_name_input.value = "";
+  amount_spent_input.value = "";
+});
 
-})
+const auth = firebase.auth();
+// Logout event listener
+logout.addEventListener("click", () => {
+  auth.signOut();
+  window.location = "login.html";
+});
