3b8ceb300ebf362ffe4cabc9bb50bf3170356fab
Moonwalker <abhisk.mhra@gmail.com>
2021-05-16 23:46:39 +0530

    firebase Auth WIP

---
+++ firebase.js
@@ -0,0 +1,20 @@
+const auth = firebase.auth();
+const input_email = document.getElementById("email");
+const input_paswd = document.getElementById("paswd");
+const submit_btn = document.getElementById("submit");
+const login_btn = document.getElementById("login");
+
+submit_btn.addEventListener("click", signIn);
+
+async function signIn(event) {
+  event.preventDefault();
+  const email = input_email.value;
+  const paswd = input_paswd.value;
+  try {
+    await auth.createUserWithEmailAndPassword(email, paswd).then((user) => {
+      console.log(user);
+    });
+  } catch (err) {
+    console.log(error);
+  }
+}
