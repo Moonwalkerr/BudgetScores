b14021e755d2d96ab3f22799ca91dd340481819a
Moonwalker <abhisk.mhra@gmail.com>
2021-05-18 21:39:18 +0530

    firebase auth with google added

--- public/js/firebaseAuth.js
+++ public/js/firebaseAuth.js
@@ -8,9 +8,11 @@ const input_email = document.getElementById("email");
 const input_paswd = document.getElementById("paswd");
 const signIn_btn = document.getElementById("signIn");
 const login_btn = document.getElementById("login");
+const google_auth = document.getElementById("google_auth");
 
 signIn_btn.addEventListener("click", signIn);
 login_btn.addEventListener("click", login);
+google_auth.addEventListener("click", signInWithGoogle);
 
 async function signIn(event) {
   event.preventDefault();
@@ -33,6 +35,16 @@ async function login(event) {
     await auth
       .signInWithEmailAndPassword(email, paswd)
       .then((user) => console.log(user));
+  } catch (err) {
+    alert(err.message);
+  }
+}
+
+async function signInWithGoogle(event) {
+  event.preventDefault();
+  let provider = new firebase.auth.GoogleAuthProvider();
+  try {
+    await auth.signInWithPopup(provider).then((res) => console.log(res.user));
   } catch (err) {
     alert(err.message);
   }
