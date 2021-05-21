4e23ee5c49c1a7451f69b90beaf8291d81eab76e
Moonwalker <abhisk.mhra@gmail.com>
2021-05-21 21:36:48 +0530

    feat : guest user sign IN  method and button added

--- public/js/firebaseAuth.js
+++ public/js/firebaseAuth.js
@@ -9,10 +9,12 @@ const input_paswd = document.getElementById("paswd");
 const signIn_btn = document.getElementById("signIn");
 const login_btn = document.getElementById("login");
 const google_auth = document.getElementById("google_auth");
+const guest_signIn = document.getElementById("guest_signIn");
 
 signIn_btn.addEventListener("click", signIn);
 login_btn.addEventListener("click", login);
 google_auth.addEventListener("click", signInWithGoogle);
+guest_signIn.addEventListener("click", signInAsGuest);
 
 async function signIn(event) {
   event.preventDefault();
@@ -32,9 +34,7 @@ async function login(event) {
   const email = input_email.value;
   const paswd = input_paswd.value;
   try {
-    await auth
-      .signInWithEmailAndPassword(email, paswd)
-      .then((user) => console.log(user));
+    await auth.signInWithEmailAndPassword(email, paswd);
   } catch (err) {
     alert(err.message);
   }
@@ -47,5 +47,18 @@ async function signInWithGoogle(event) {
     await auth.signInWithPopup(provider).then((res) => console.log(res.user));
   } catch (err) {
     alert(err.message);
+  }
+}
+
+async function signInAsGuest(event) {
+  event.preventDefault();
+  let guest_email = "testUser@test.com";
+  let guest_paswd = 123456;
+  input_email.value = guest_email;
+  input_paswd.value = guest_paswd;
+  try {
+    await auth.signInWithEmailAndPassword(guest_email, guest_paswd);
+  } catch (error) {
+    alert(error.message);
   }
 }
